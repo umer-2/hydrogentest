5b038584d6e1ae5e66366d226ab004f41309cac1
Umer Rafique <rafiqueumer985@gmail.com>
2024-07-09 16:51:32 +0500

    Hello

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -20,6 +20,7 @@ export function Header({header, isLoggedIn, cart, publicStoreDomain}) {
         publicStoreDomain={publicStoreDomain}
       />
       <HeaderCtas isLoggedIn={isLoggedIn} cart={cart} />
+      <h1>Hello</h1>
     </header>
   );
 }
